af00e9c027daf14dc705928046bd7d6aa1ea2427
Miyuki Tsai <miyuki@MiyuMBA.local>
2023-10-13 06:03:15 +0800

    feat: connect Apollo Client to Vue

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { createApp, h, provide } from 'vue'
 import { createPinia } from 'pinia'
 
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { apolloClient } from './graphql/apolloClient'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient)
+  },
+
+  render: () => h(App),
+})
 
 app.use(createPinia())
 app.use(router)
